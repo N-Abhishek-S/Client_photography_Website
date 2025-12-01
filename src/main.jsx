0bbab076d154f1a5205f3559373936dd52382649
N-Abhishek-s <nagargojeabhishek96@gmail.com>
2025-12-01 20:53:24 +0530

    Update the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
+// src/main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
 import { HashRouter, Route, Routes } from "react-router-dom";
+
 import "./index.css";
+
 import App from "./App.jsx";
 import HomePage from "./components/HomePage.jsx";
 import CourseSalesPage from "./components/CourseSalesPage.jsx";
@@ -14,22 +16,22 @@ import BookNow from "./components/BookNow.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <HashRouter>
-        <Routes>
-          {/* App component should contain Header, Footer and Outlet */}
-          <Route path="/" element={<App />}>
-            <Route index element={<HomePage />} />
-            <Route path="courses" element={<CourseSalesPage />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="services" element={<Services />} />
-<Route path="portfolio" element={<Owner3DPortfolio />} /> 
-<Route path="booknow" element={<BookNow />} /> 
-            {/* Remove header and footer routes - they're part of App layout */}
-          </Route>
-          {/* Optional: catch-all 404 */}
-          {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
-      </HashRouter>
+    <HashRouter>
+      <Routes>
+        {/* App is the main layout (Header, Footer, Outlet) */}
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="courses" element={<CourseSalesPage />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="services" element={<Services />} />
+          <Route path="portfolio" element={<Owner3DPortfolio />} />
+          <Route path="booknow" element={<BookNow />} />
+        </Route>
+
+        {/* Optional: 404 page */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </HashRouter>
   </StrictMode>
 );
