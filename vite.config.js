0095b1e954461647834796ae9234acc8eee63aa3
N-Abhishek-s <nagargojeabhishek96@gmail.com>
2025-12-01 16:01:32 +0530

    Update pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Client_photography_Website/', // 👈 important for GitHub Pages
 })
